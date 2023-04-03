59e02997925be2a6825e07c332b867125059f98f
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-04-03 23:25:01 +0200

    rendered products, added product images and fixed some bugs

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class Customer(models.Model):
@@ -15,9 +16,19 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
-    #image
+    image = models.ImageField(null=True, blank=True)
+
+
     def __str__(self):
         return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
     
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
@@ -34,8 +45,7 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return str(self.id)
+ 
     
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
